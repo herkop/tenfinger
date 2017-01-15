b10c30125b13d8b2f9bacb51a98f878a57b02acf
Herko Pirk <herko.pirk@gmail.com>
2017-01-15 18:07:53 +0200

    created service for exercises

--- app/controller/maincontroller.js
+++ app/controller/maincontroller.js
@@ -1,57 +1,95 @@
-//var angular = require('angular');
 var app = angular.module('TenFingers');
 
 app.controller('MainController', function($scope) {
 
 });
 
-app.controller('ExController', function($scope, $location, $stateParams) {
+app.controller('ExerciseController', function($scope, keyboard) {
+
+    $scope.keyboard = keyboard;
+    $scope.onKeyDown = function ($event) {
+        $scope.result = keyboard.getWord();
+
+        keyboard.letterTyped($event.key);
+        $scope.keyboard.letter_active = keyboard.getActiveLetter();
+        $scope.keyboard.key_active = keyboard.getActiveKey();
+        $scope.letter_style = keyboard.getLetterStyle();
+        $scope.key_wrong = keyboard.getWrongKey();
+
+    };
+});
+
+app.controller('MenuController', function($scope, $location){
     $scope.active = function(loc){
         return loc == $location.path();
     };
-    var step = 0;
+});
+
+app.controller('ExController', function($scope, $stateParams, keyboard) {
+    $scope.keyboard = keyboard;
     var exes = ["jfjffjjfjjjfffjfjfjjfjff", "dkddkkkddkdkdkdkkkdddkd", "slllssllsllsssllsllslslsl", "aöaöaöaöaöööaaöaöaööaöö", "sösdjklalkdfjjllskkdjjfalsddkjfds"];
-    $scope.keys = " No key"
-    //if(angular.isUndefined($scope.result)) {
-        //$scope.result = [];
-        $scope.result = exes[0];
-        step = 0;
-    //}
-    /**if(!angular.isUndefined($stateParams.number)) {
+
+    if(!angular.isUndefined($stateParams.number)) {
+        $scope.number = $stateParams.number;
         $scope.result = exes[$stateParams.number-1];
+        keyboard.setWord(exes[$stateParams.number-1]);
+
+        $scope.keyboard.letter_active = keyboard.getActiveLetter();
+        $scope.keyboard.key_active = keyboard.getActiveKey();
+        $scope.letter_style = keyboard.getLetterStyle();
+
+    }
+
+});
+
+app.service('keyboard', function(){
+    var step = 0;
+    var word = null;
+    var active_letter = null;
+    var active_key = null;
+    var letter_style = [];
+    var wrong_key = null;
+    this.getWord = function(){
+        return word;
+    };
+    this.setWord = function(value){
+        word = value;
         step = 0;
-        $scope.key_active = $scope.result[step];
-        $scope.letter_active = $scope.result[step]+step;
-        $scope.letter_style = [];
-            //$scope.key_wrong = "";
+        active_key = word[step];
+        active_letter = word[step]+step;
+        letter_style = [];
+        wrong_key = null;
+    };
+    this.getActiveLetter = function(){
+        return active_letter;
+    };
+    this.getActiveKey = function(){
+        return active_key;
+    };
+    this.getLetterStyle = function(){
+        return letter_style;
+    };
+    this.getWrongKey = function(){
+        return wrong_key;
+    };
 
-    }*/
-        if(angular.isUndefined($scope.key_active) && step == 0) {
-            $scope.key_active = $scope.result[step];
-        }
-        if(angular.isUndefined($scope.letter_active) && step == 0) {
-            $scope.letter_active = $scope.result[step]+step;
-        }
-        if(angular.isUndefined($scope.letter_style)) {
-            $scope.letter_style = [];
-        }
-
-        $scope.onKeyDown = function ($event) {
-            if ($scope.result[step] == $event.key) {
-                if($scope.key_wrong == "" || angular.isUndefined($scope.key_wrong)) {
-                    $scope.letter_style[step] = true;
-                }
-                step += 1;
-
-                $scope.key_wrong = "";
-                $scope.key_active = $scope.result[step];
-                $scope.letter_active = $scope.result[step]+step;
-
+    this.letterTyped = function(letter){
+        if(letter == word[step]){
+            //correct - next letter
+            if(wrong_key == null) {
+                letter_style[step] = true;
             }
-            else {
-                $scope.letter_style[step] = false;
-                $scope.key_wrong = $event.key;
-            }
+
+            step += 1;
+            wrong_key = null;
+            active_key = word[step];
+            active_letter = word[step]+step;
         }
+        else{
+            //wrong - red color
+            letter_style[step] = false;
+            wrong_key = letter;
+        }
+    };
 
 });
