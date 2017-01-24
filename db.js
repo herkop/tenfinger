dd6509ab3fe7b56014e53bae1b41ea7e572209f1
Herko <herko.pirk@gmail.com>
2017-01-24 23:52:45 +0200

    creating database connection

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ var pg = require('pg');
 
 //pg.defaults.ssl = true;
 pg.connect(process.env.DATABASE_URL, function (err, client) {
+    console.log("Starting");
     if (!err){
-        console.log("Connected to databse");
+        console.log("Connected to database");
     }
     else{
+        console.log("error!!");
         throw err;
     }
 });
