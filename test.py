ad5988176acc246353bcfd597339e965c8101587
Herko Pirk <herko.pirk@gmail.com>
2017-04-07 10:16:07 +0300

    testing py

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import sys
-//from estnltk import Text
+# from estnltk import Text
 
 inputText = sys.argv[1]
-//text = Text(inputText)
-//sentences = text.sentence_texts
+# text = Text(inputText)
+# sentences = text.sentence_texts
 print(inputText)
 sys.stdout.flush()
